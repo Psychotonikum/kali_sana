66cf5634c59125fdcd0bf7dbabe61bee9e9d19e7
root <root@localhost.localdomain>
2015-10-22 19:39:35 +0200

    postinstall script udpate

--- newInstall.sh
+++ newInstall.sh
@@ -1,9 +1,11 @@
+# copy files
+cp .bashrc ~/
+cp .bash_aliases ~/
+cp .emacs ~/ 
+
 # add architecture
 dpkg --add-architecture i386
 dpkg --add-architecture x64
-
-#install x64 headers
-sudo apt-get install linux-headers-$(uname -r) -y
 
 #update & upgrade
 apt-get clean
@@ -11,17 +13,18 @@ apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
 
-#install utils
-apt-get install -y emacs
-apt-get install -y flashplugin-nonfree
-apt-get install -y conky
-apt-get install -y gdebi
-apt-get install -y alsa-utils
-apt-get install -y tor
-apt-get install -y filezilla filezilla-common
-apt-get install -y htop nethogs
-apt-get install -y gtk-recordmydesktop recordmydesktop remmina
-apt-get install -y virtualbox virtualbox-dkms
+# install stuff
+apt-get install -y \
+	linux-headers-$(uname -r)
+	emacs \
+	lashplugin-nonfree \
+	conky \
+	gdebi \
+	alsa-utils \
+	tor \
+	filezilla filezilla-common \
+	htop nethogs \
+	virtualbox virtualbox-dkms
 
 #install skype
 wget -O skype-install.deb http://www.skype.com/go/getskype-linux-deb
@@ -33,6 +36,3 @@ rm skype-install.deb
 apt-get autoclean
 apt-get autoremove
 apt-get purge
-
-# write ~/.bash_aliases
-echo "alias emacs='emacs -nw'" >> ~/.bash_aliases
